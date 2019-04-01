0985df2e2f6f2ba3f2ab0ce07d090d2c18a4aa71
Josh Halvorson <joshhalvorson16@gmail.com>
2019-04-01 09:25:37 -0700

    Add csv conversion

--- app/src/main/java/com/joshuahalvorson/journal/JournalEntry.java
+++ app/src/main/java/com/joshuahalvorson/journal/JournalEntry.java
@@ -27,6 +27,36 @@ public class JournalEntry implements Serializable {
         this.date = "";
     }
 
+    public JournalEntry(String csvString){
+        String[] values = csvString.split(",");
+        if(values.length == 5){
+            try {
+                this.id = Integer.parseInt(values[0]);
+            }catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+            this.date = values[1];
+            try {
+                this.rating = Integer.parseInt(values[2]);
+            }catch (NumberFormatException e ){
+                e.printStackTrace();
+            }
+            this.entryText = values[3].replace("~@", ",");
+            this.imageUri = values[4].equals("unused") ? "" : values[4];
+        }
+    }
+
+    public String toCsvString(){
+        return String.format(
+                "%d,%s,%d,%s,%s",
+                id,
+                date,
+                rating,
+                entryText.replace(",", "~@"),
+                imageUri == "" ? "unused" : imageUri
+                );
+    }
+
     public String getEntryText() {
         return entryText;
     }
